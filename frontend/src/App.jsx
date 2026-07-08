65850ad4e2f25c6537dc46b64f40ca712c7c65fe
Ashish Patel <ashishkumarr0856@gmail.com>
2026-07-08 13:05:33 +0530

    riding page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import UserProtectWrapper from './pages/UserProtectWrapper'
 import UserLogout from './pages/UserLogout'
 import CaptainHome from './pages/CaptainHome'
 import CaptainProtectWrapper from './pages/CaptainProtectWrapper'
+import Riding from './pages/Riding'
 
 const App = () => {
   return (
@@ -29,6 +30,7 @@ const App = () => {
         } />
 
         <Route path="/login" element={<UserLogin/>} />
+        <Route path='/riding' element={<Riding/>}/>
         <Route path="/signup" element={<UserSignup />} />
         <Route path="/captain-signup" element={<CaptainSignup />} />
         <Route path="/captain-login" element={<Captainlogin />} />
