c9bc3752d25c8d389183a6af53d68785ea5883de
Ashish Patel <ashishkumarr0856@gmail.com>
2026-07-09 11:39:06 +0530

    riding ui

--- frontend/src/pages/CaptainHome.jsx
+++ frontend/src/pages/CaptainHome.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const CaptainHome = () => {
   return (
-    <div>CaptainHome</div>
+    <div className='h-screen'>
+      <div className='fixed p-6 top-0 flex items-center justify-between w-screen'>
+        <img className='w-16' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+        <Link to='/captain-home' className=' h-10 w-10 bg-white flex items-center justify-center rounded-full'>
+          <i className="text-lg font-medium ri-logout-box-r-line"></i>
+        </Link>
+      </div>
+      <div className='h-3/5'>
+        <img className='h-full w-full object-cover' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="" />
+      </div>
+
+      <div className='1/2 p-4'>
+        <div>
+          <div>
+            <img src="" alt="" />
+            <h4>Ashish Patel</h4>
+          </div>
+          <div>
+            <h4>295.12</h4>
+            <p>Earned</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
--- frontend/src/pages/Riding.jsx
+++ frontend/src/pages/Riding.jsx
@@ -1,26 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+
 const Riding = () => {
 
-        socket.on("ride-ended", () => {
-        navigate('/home')
-    })
+//         socket.on("ride-ended", () => {
+//         navigate('/home')
+//     })
     
     return (
         <div className='h-screen'>
-            <Link to='/home' className='fixed right-2 top-2 h-10 w-10 bg-white flex items-center justify-center rounded-full'>
+            <Link to='/home'  className='fixed right-2 top-2 h-10 w-10 bg-white flex items-center justify-center rounded-full'>
                 <i className="text-lg font-medium ri-home-5-line"></i>
             </Link>
             <div className='h-1/2'>
-                <LiveTracking />
+                {/* <LiveTracking /> */}
 
             </div>
             <div className='h-1/2 p-4'>
                 <div className='flex items-center justify-between'>
                     <img className='h-12' src="https://swyft.pl/wp-content/uploads/2023/05/how-many-people-can-a-uberx-take.jpg" alt="" />
                     <div className='text-right'>
-                        <h2 className='text-lg font-medium capitalize'>{ride?.captain.fullname.firstname}</h2>
-                        <h4 className='text-xl font-semibold -mt-1 -mb-1'>{ride?.captain.vehicle.plate}</h4>
+                        <h2 className='text-lg font-medium capitalize'>Ashish Patel</h2>
+                        <h4 className='text-xl font-semibold -mt-1 -mb-1'>MP AB7075</h4>
                         <p className='text-sm text-gray-600'>Maruti Suzuki Alto</p>
 
                     </div>
@@ -33,13 +34,13 @@ const Riding = () => {
                             <i className="text-lg ri-map-pin-2-fill"></i>
                             <div>
                                 <h3 className='text-lg font-medium'>562/11-A</h3>
-                                <p className='text-sm -mt-1 text-gray-600'>{ride?.destination}</p>
+                                <p className='text-sm -mt-1 text-gray-600'>Karnatka,bhopal</p>
                             </div>
                         </div>
                         <div className='flex items-center gap-5 p-3'>
                             <i className="ri-currency-line"></i>
                             <div>
-                                <h3 className='text-lg font-medium'>₹{ride?.fare} </h3>
+                                <h3 className='text-lg font-medium'>₹193.20</h3>
                                 <p className='text-sm -mt-1 text-gray-600'>Cash Cash</p>
                             </div>
                         </div>
